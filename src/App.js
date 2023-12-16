c7c307fd05d4a9072d97030878c6689c2a6e3112
MegablasterMan1 <tryjhnsn12@gmail.com>
2023-12-15 21:42:18 -0600

    Added Code for App.js
    Added imports and comments to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useReducer} from 'react'; // A default import
+import { API } from 'aws-amplify'; // Gives access to use to use the GraphQL
+import { List } from 'antd'; // CSS For styles
+import 'antd/dist/antd.css';
+import { listNotes } from './graphql/queries'; // Queries from GraphQL Queries
 
-function App() {
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_NOTES': //IF NO ERROR
+      return {
+        ...state,
+        notes: action.notes,
+        loading: false
+      };
+    case 'ERROR': //IF ERROR
+      return {
+        ...state,
+        loading: false,
+        error: true
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchNotes = async() => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      });
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
+    } catch (err) {
+      console.log('error: ', err);
+      dispatch({ type: 'ERROR' });
+    }
+  };
+
+  useEffect(() => {fetchNotes()}, []); //Pull up notes when the display loads up.
+
+  const styles = { // Custom Styles
+    container: {padding: 20}
+    ,input: {marginBottom: 10}
+    ,item: { textAlign: 'left' }
+    ,p: { color: '#1890ff' }
+  };
+
+  const renderItem = (item) => {
+    return (
+      <List.Item style={styles.item}>
+      <List.Item.Meta
+        title={item.name}
+        description={item.description}
+      />
+      </List.Item>
+    );
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={styles.container}>
+
+        <List
+          loading={state.loading}
+          dataSource={state.notes}
+          renderItem={renderItem}
+        />
+
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+/*
+NOTESDATA (Sample Data)
+{
+  "data": {
+    "listNotes": {
+      "items": [
+        {
+          "id": "3fb75361-70a9-4877-be67-70ba0eac112a",
+          "name": "Book flight",
+          "description": "Flying to Paris on June 1 returning June 10",
+          "completed": false
+        }
+      ]
+    }
+  }
+}
+
+*/
